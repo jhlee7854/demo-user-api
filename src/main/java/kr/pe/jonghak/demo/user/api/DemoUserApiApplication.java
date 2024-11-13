3c3b1eed3f7ad2282522e4ac3612e86bc8e7d2f9
jhlee7854 <jhlee7854@me.com>
2024-11-13 14:16:56 +0900

    Initial commit

---
+++ src/main/java/kr/pe/jonghak/demo/user/api/DemoUserApiApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.jonghak.demo.user.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/pe/jonghak/demo/user/api/DemoUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.jonghak.demo.user.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
