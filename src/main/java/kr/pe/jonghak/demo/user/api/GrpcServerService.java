fcef2297e3db961a3e8233ccd792ccdd643c1c65
jhlee7854 <jhlee7854@me.com>
2025-01-09 20:19:59 +0900

    MongoDB를 이용한 사용자 조회 기능 추가

--- src/main/java/kr/pe/jonghak/demo/user/api/GrpcServerService.java
+++ src/main/java/kr/pe/jonghak/demo/user/api/GrpcServerService.java
@@ -3,21 +3,31 @@ package kr.pe.jonghak.demo.user.api;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+
+import java.util.Optional;
 
 @GrpcService
 @Slf4j
 public class GrpcServerService extends UserServiceGrpc.UserServiceImplBase {
+    private final UserRepository userRepository;
+
+    public GrpcServerService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public void getUser(UserRequest request, StreamObserver<UserReply> responseObserver) {
         String name = request.getName();
         log.info("requested user name: {}", name);
 
-        // TODO: mongoDB에서 사용자를 조회한 후 해당 사용자 정보를 응답 정보로 사용한다.
+        Optional<User> user = userRepository.findOne(Example.of(new User(null, request.getName(),  null)));
+        User foundUser = user.orElseThrow();
         UserReply reply = UserReply.newBuilder()
-            .setId("testId")
-            .setName(name)
-            .setEmail("testEmail")
+            .setId(foundUser.getId())
+            .setName(foundUser.getName())
+            .setEmail(foundUser.getEmail())
             .build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
